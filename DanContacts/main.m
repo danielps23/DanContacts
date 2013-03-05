0d39ed27df3c6ec973425a45d64356c89772d204
ios3345 <ios3345@Caelums-Mac-mini-2.local>
2013-03-05 11:59:13 -0300

    Initial Commit

---
+++ DanContacts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DanContacts
+//
+//  Created by ios3345 on 05/03/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EMBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EMBAppDelegate class]));
+    }
+}
